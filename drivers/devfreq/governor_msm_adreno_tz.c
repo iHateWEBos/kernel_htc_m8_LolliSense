9c9ef18d09b89fee55107a77da9cdacc963c7543
arter97 <qkrwngud825@gmail.com>
2015-07-01 09:37:56 -0400

    Adreno idler for devfreq-based Adreno devices
    Adreno idler by arter97 - Idling algorithm,
    an efficient workaround for msm-adreno-tz's overheads.
    
    Main goal is to lower the power consumptions while maintaining high-performance.
    
    Since msm-adreno-tz tends to *not* use the lowest frequency even on idle,
    Adreno idler replaces msm-adreno-tz's algorithm when it comes to
    calculating idle frequency(mostly by ondemand's method).
    The higher frequencies are not touched with this algorithm, so high-demanding
    games will (most likely) not suffer from worsened performance.
    
    The additional idle_lasttime detects if last 500ms was idle before
    ramping down the frequency to prevent micro-lags on scrolling or playing games.
    
    Version 1.1
    
    modified for the m8 by ihatewebos
    Signed-off-by: ihatewebos <mannyb1877@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -22,17 +22,35 @@
 #include <mach/scm.h>
 #include "governor.h"
 
+extern bool mdss_screen_on;
+
 static DEFINE_SPINLOCK(tz_lock);
 
-#define FLOOR			5000
+/*
+ * FLOOR is 5msec to capture up to 3 re-draws
+ * per frame for 60fps content.
+ */
+#define FLOOR		        5000
+/*
+ * MIN_BUSY is 1 msec for the sample to be sent
+ */
+#define MIN_BUSY		1000
 #define LONG_FLOOR		50000
 #define HIST			5
 #define TARGET			80
 #define CAP			75
-#define BUSY_BIN	95
-#define LONG_FRAME	25000
+/*
+ * Use BUSY_BIN to check for fully busy rendering
+ * intervals that may need early intervention when
+ * seen with LONG_FRAME lengths
+ */
+#define BUSY_BIN		95
+#define LONG_FRAME		25000
 
-
+/*
+ * CEILING is 50msec, larger than any standard
+ * frame length, but less than the idle timer.
+ */
 #define CEILING			50000
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
@@ -40,11 +58,12 @@ static DEFINE_SPINLOCK(tz_lock);
 
 #define TAG "msm_adreno_tz: "
 
+/* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
 {
 	int ret;
 	spin_lock(&tz_lock);
-	
+	/* sync memory before sending the commands to tz*/
 	__iowmb();
 	ret = scm_call_atomic2(SCM_SVC_IO, cmd, val1, val2);
 	spin_unlock(&tz_lock);
@@ -55,7 +74,7 @@ static int __secure_tz_entry3(u32 cmd, u32 val1, u32 val2, u32 val3)
 {
 	int ret;
 	spin_lock(&tz_lock);
-	
+	/* sync memory before sending the commands to tz*/
 	__iowmb();
 	ret = scm_call_atomic3(SCM_SVC_IO, cmd, val1, val2, val3);
 	spin_unlock(&tz_lock);
@@ -74,6 +93,10 @@ static void _update_cutoff(struct devfreq_msm_adreno_tz_data *priv,
 	}
 }
 
+#ifdef CONFIG_ADRENO_IDLER
+extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
+		 unsigned long *freq);
+#endif
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				u32 *flag)
 {
@@ -99,8 +122,31 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		return result;
 	}
 
+	/* Prevent overflow */
+	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
+		stats.busy_time >>= 7;
+		stats.total_time >>= 7;
+	}
+
 	*freq = stats.current_frequency;
 	*flag = 0;
+
+	/*
+	 * Force to use & record as min freq when system has
+	 * entered pm-suspend or screen-off state.
+	 */
+	if (!mdss_screen_on) {
+		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
+		return 0;
+	}
+
+#ifdef CONFIG_ADRENO_IDLER
+	if (adreno_idler(stats, devfreq, freq)) {
+		/* adreno_idler has asked to bail out now */
+		return 0;
+	}
+#endif
+
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
 	if (priv->bus.num) {
@@ -110,9 +156,16 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		priv->bus.ram_time += b.ram_wait;
 	}
 
+	/*
+	 * Do not waste CPU cycles running this algorithm if
+	 * the GPU just started, or if less than FLOOR time
+	 * has passed since the last run or the gpu hasn't been
+	 * busier than MIN_BUSY.
+	 */
 	if ((stats.total_time == 0) ||
-		(priv->bin.total_time < FLOOR)) {
-		return 1;
+		(priv->bin.total_time < FLOOR) ||
+		(unsigned int) priv->bin.busy_time < MIN_BUSY) {
+		return 0;
 	}
 
 	if ((stats.busy_time * 100 / stats.total_time) > BUSY_BIN) {
@@ -125,7 +178,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	}
 
 	level = devfreq_get_freq_level(devfreq, stats.current_frequency);
-
 	if (level < 0) {
 		pr_err(TAG "bad freq %ld\n", stats.current_frequency);
 		return level;
@@ -149,6 +201,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
 
+	/*
+	 * If the decision is to move to a different level, make sure the GPU
+	 * frequency changes.
+	 */
 	if (val) {
 		level += val;
 		level = max(level, 0);
@@ -162,17 +218,24 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 			(unsigned int) priv->bus.total_time;
 	gpu_percent = (100 * (unsigned int)priv->bus.gpu_time) /
 			(unsigned int) priv->bus.total_time;
+
+	/*
+	 * If there's a new high watermark, update the cutoffs and send the
+	 * FAST hint.  Otherwise check the current value against the current
+	 * cutoffs.
+	 */
 	if (norm_cycles > priv->bus.max) {
 		_update_cutoff(priv, norm_cycles);
 		*flag = DEVFREQ_FLAG_FAST_HINT;
 	} else {
+		/* GPU votes for IB not AB so don't under vote the system */
 		norm_cycles = (100 * norm_cycles) / TARGET;
 		act_level = priv->bus.index[level] + b.mod;
 		act_level = (act_level < 0) ? 0 : act_level;
 		act_level = (act_level >= priv->bus.num) ?
 			(priv->bus.num - 1) : act_level;
 		if (norm_cycles > priv->bus.up[act_level] &&
-			gpu_percent > CAP)
+				gpu_percent > CAP)
 			*flag = DEVFREQ_FLAG_FAST_HINT;
 		else if (norm_cycles < priv->bus.down[act_level] && level)
 			*flag = DEVFREQ_FLAG_SLOW_HINT;
@@ -200,7 +263,7 @@ static int tz_notify(struct notifier_block *nb, unsigned long type, void *devp)
 		result = update_devfreq(devfreq);
 		mutex_unlock(&devfreq->lock);
 		break;
-	
+	/* ignored by this governor */
 	case ADRENO_DEVFREQ_NOTIFY_SUBMIT:
 	default:
 		break;
@@ -236,10 +299,12 @@ static int tz_start(struct devfreq *devfreq)
 	ret = scm_call(SCM_SVC_DCVS, TZ_INIT_ID, tz_pwrlevels,
 			sizeof(tz_pwrlevels), NULL, 0);
 
-	if (ret != 0)
+	if (ret != 0) {
 		pr_err(TAG "tz_init failed\n");
+		return ret;
+	}
 
-	
+	/* Set up the cut-over percentages for the bus calculation. */
 	if (priv->bus.num) {
 		for (i = 0; i < priv->bus.num; i++) {
 			t1 = (u32)(100 * priv->bus.ib[i]) /
@@ -248,7 +313,7 @@ static int tz_start(struct devfreq *devfreq)
 			priv->bus.p_down[i] = t2 - 2 * HIST;
 			t2 = t1;
 		}
-		
+		/* Set the upper-most and lower-most bounds correctly. */
 		priv->bus.p_down[0] = 0;
 		priv->bus.p_down[1] = (priv->bus.p_down[1] > (2 * HIST)) ?
 					priv->bus.p_down[1] : (2 * HIST);
@@ -316,7 +381,7 @@ static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
 		break;
 
 	case DEVFREQ_GOV_INTERVAL:
-		
+		/* ignored, this governor doesn't use polling */
 	default:
 		result = 0;
 		break;
